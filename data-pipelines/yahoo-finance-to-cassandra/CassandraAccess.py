432329d90d7b3e65b5a87be9e10008b041eab88d
Ananth Tirumanur <akrish1982@gmail.com>
2023-07-08 15:16:14 -0400

    Update CassandraAccess.py

--- data-pipelines/yahoo-finance-to-cassandra/CassandraAccess.py
+++ data-pipelines/yahoo-finance-to-cassandra/CassandraAccess.py
@@ -18,7 +18,7 @@ class CassandraAccess:
         ssl_context = SSLContext(PROTOCOL_TLSv1_2 )
         ssl_context.load_verify_locations('/home/akrish1982/data-engineering/data-pipelines/sf-class2-root.crt')
         ssl_context.verify_mode = CERT_REQUIRED
-        auth_provider = PlainTextAuthProvider(username='data-egineering-at-163926246741', password='jgjevjCy26Ue1xrJ5ctdJlIUT95ltVPgbcHVVVQHme0=')
+        auth_provider = PlainTextAuthProvider(username='data-egineering-at-', password='=')
         profile = ExecutionProfile(
             consistency_level=ConsistencyLevel.LOCAL_QUORUM,
             retry_policy=KeyspacesRetryPolicy(RETRY_MAX_ATTEMPTS=5))
